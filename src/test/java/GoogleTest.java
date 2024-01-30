fa73252df05f81d13497f38c7249265fdb560116
Студент Академии <std@itstep.loc>
2024-01-30 19:20:36 +0300

    актуализировал Google тест

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,22 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleTest {
 
     public static void main(String[] args) {
 
-//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\std\\Downloads\\chromedriver-win64\\chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-//
-//        driver.get("https://www.google.ru/");
-//
-//        WebElement googleInput = driver.findElement(By.cssSelector(".gLFyf"));
-//
-//        googleInput.sendKeys("Фильмы");
-//        googleInput.sendKeys(Keys.ENTER);
-//
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\std\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.ru/");
+
+        WebElement googleInput = driver.findElement(By.cssSelector(".gLFyf"));
+
+        googleInput.sendKeys("Фильмы");
+        googleInput.sendKeys(Keys.ENTER);
+
         String a = "asda";
 
-        assert 1 == 2 : "!@#";
-
-//        driver.close();
+        driver.close();
     }
 
 

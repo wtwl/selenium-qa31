3954bfcd934b44e3903ac7d005623be0dc4c3e34
Студент Академии <std@itstep.loc>
2024-01-30 20:56:39 +0300

    добавил тест на селект

---
+++ src/test/java/SelectTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\std\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+        WebElement dropdown = driver.findElement(By.cssSelector("#dropdown"));
+        Select dropdownSelect = new Select(dropdown);
+
+        dropdownSelect.selectByVisibleText("Option 2");
+
+        driver.quit();
+    }
+}
